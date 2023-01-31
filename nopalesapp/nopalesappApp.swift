e1c7abd22f550b96695e255875491195d89c0bad
Cameron <cjohnson22@msu.idserve.net>
2023-01-31 09:18:24 -0500

    Initial Commit

---
+++ nopalesapp/nopalesappApp.swift
@@ -0,0 +1,17 @@
+//
+//  nopalesappApp.swift
+//  nopalesapp
+//
+//  Created by Cameron Johnson on 1/31/23.
+//
+
+import SwiftUI
+
+@main
+struct nopalesappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
