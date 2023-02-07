2581244fcffe185222927ab212aa86933665998d
Duke Holsing <dh021804@gmail.com>
2023-02-07 11:52:44 -0500

    SearchScreen

---
+++ nopalesapp/SearchScreen.swift
@@ -0,0 +1,20 @@
+//
+//  SearchScreen.swift
+//  nopalesapp
+//
+//  Created by Duke Holsing on 2/7/23.
+//
+
+import SwiftUI
+
+struct SearchScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SearchScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchScreen()
+    }
+}
