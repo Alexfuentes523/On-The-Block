3a30758ec736f08a2f9ceb573c1a3376021b605f
Cameron <cjohnson22@msu.idserve.net>
2023-02-07 11:44:46 -0500

    Small changes to the main screen

--- nopalesapp/ContentView.swift
+++ nopalesapp/ContentView.swift
@@ -8,39 +8,100 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-   
-            ZStack {
+        var eggShell = #colorLiteral(red: 0.9302913547, green: 0.9253246188, blue: 0.916793704, alpha: 1)
+        ZStack {
+            
+            Color(eggShell)
+                .ignoresSafeArea()
+            
+            
+            
+            
+            ScrollView {
                 
-                Color(.gray)
-                    .ignoresSafeArea()
-                
-                ScrollView {
-                    
                 VStack {
                     
-                    HStack{
-                        Image("blm Barber")
+                    HStack {
+                        Image(systemName: "plus")
                             .padding()
-                            .scaledToFit()
-                            .offset(x: -10 ,y: 50)
-                        Image("dime")
+                        Text("ON THE BLOCK")
                             .padding()
-                            .scaledToFit()
+                        Image(systemName:"magnifyingglass")
+                            .padding()
+                        
                     }
-                    //                HStack {
-                    //                    Image("thecity")
-                    //                        .padding()
-                    //                    Image("")
-                    //                }
+                    
+                    HStack {
+                        VStack {
+                            Image(systemName: "cup.and.saucer.fill")
+                                .padding(0.5)
+                            Text("Coffee Shop")
+                                .font(.system(size: 7))
+                        }
+                        .padding(0.4)
+                        VStack {
+                            Image(systemName: "birthday.cake.fill")
+                                .padding(0.5)
+                            Text("Desserts")
+                                .font(.system(size: 7))
+                        }
+                        .padding(0.4)
+                        VStack {
+                            Image(systemName: "eyebrow")
+                                .padding(0.5)
+                            Text("Salon")
+                                .font(.system(size: 7))
+                        }
+                        .padding(0.4)
+                        VStack {
+                            Image(systemName: "takeoutbag.and.cup.and.straw.fill")
+                                .padding(0.5)
+                            Text("Takeout")
+                                .font(.system(size: 7))
+                        }
+                        .padding(0.4)
+                        VStack {
+                            Image(systemName: "wrench.adjustable.fill")
+                                .padding(0.5)
+                            Text("Repair")
+                                .font(.system(size: 7))
+                        }
+                        .padding(0.4)
+                        VStack {
+                            Image(systemName: "fork.knife.circle.fill")
+                                .padding(0.5)
+                            Text("Restaruant")
+                                .font(.system(size: 7))
+                        }
+                        .padding(0.4)
+                    }
+                    
+                    
+                    }
+                    //HStack{
+                    //Image("blm Barber")
+                    //.padding()
+                    //.scaledToFit()
+                    //.offset(x: -10 ,y: 50)
+                    //Image("dime")
+                    //.padding()
+                    //.scaledToFit()
                 }
-                
+                //                HStack {
+                //                    Image("thecity")
+                //                        .padding()
+                //                    Image("")
+                //                }
             }
+            
         }
     }
+    
     
     struct ContentView_Previews: PreviewProvider {
         static var previews: some View {
             ContentView()
         }
     }
-}
+    
+
